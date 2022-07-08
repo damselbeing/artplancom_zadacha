8a3f2225bce18f5145676fefea28cf2081d18936
damselbeing <damselbeing@gmail.com>
2022-07-08 11:59:16 +0300

    README file was added.

--- src/main/java/zadacha/controllers/AnimalController.java
+++ src/main/java/zadacha/controllers/AnimalController.java
@@ -1,7 +1,6 @@
 package zadacha.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import zadacha.entities.Animal;
 import zadacha.exceptions.AnimalNotFoundException;
