6bf2e98ad0c913e8f023c7e4bff6f9dc107a8abf
damselbeing <damselbeing@gmail.com>
2022-07-10 17:34:50 +0300

    GlobalExceptionHandler was added.

---
+++ src/main/java/zadacha/exceptions/ErrorMessage.java
@@ -0,0 +1,21 @@
+package zadacha.exceptions;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class ErrorMessage {
+    private int statusCode;
+    private Date timestamp;
+    private String message;
+
+    public ErrorMessage(int statusCode,
+                        Date timestamp,
+                        String message) {
+        this.statusCode = statusCode;
+        this.timestamp = timestamp;
+        this.message = message;
+    }
+
+}
