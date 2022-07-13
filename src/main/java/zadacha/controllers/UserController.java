dd12cb1b11a89439ee1e1c297e0395c341ccaae7
damselbeing <damselbeing@gmail.com>
2022-07-13 16:36:08 +0300

    Login logic was improved.

--- src/main/java/zadacha/controllers/UserController.java
+++ src/main/java/zadacha/controllers/UserController.java
@@ -3,6 +3,7 @@ package zadacha.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import zadacha.entities.User;
+import zadacha.exceptions.RequestException;
 import zadacha.exceptions.UserAlreadyExistsException;
 import zadacha.exceptions.UserNotFoundException;
 import zadacha.exceptions.WrongPasswordException;
@@ -35,10 +36,19 @@ public class UserController {
     }
 
     @PutMapping("/login")
-    public boolean accessAccount(@RequestBody User user)
-            throws UserNotFoundException, WrongPasswordException {
+    public boolean accessAccount(@RequestBody User user,
+                                 HttpServletRequest request)
+            throws UserNotFoundException, WrongPasswordException, RequestException {
 
-        return userService.loginUser(user);
+        String username = user.getName();
+        String password = user.getPassword();
+
+        if(userService.loginUser(user)) {
+            userService.authWithHttpServletRequest(request, username, password);
+            return true;
+        }
+
+        return false;
     }
 
     @GetMapping("/login")
